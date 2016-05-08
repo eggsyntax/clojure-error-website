8f82feb18987f1658426ba8635ce8ad9fa6ac838
Egg Syntax <egg@novonon.com>
2016-05-07 21:19:53 -0400

    Cleanup; correct links to error pages

--- src/clj/error_website/github_reader.clj
+++ src/clj/error_website/github_reader.clj
@@ -5,11 +5,14 @@
             [hickory.select :as hs]))
 
 
+;; Base URL for github
+(def site "https://github.com/")
+
+;; Base URL for repo
+(def base (str site "yogthos/clojure-error-message-catalog"))
+
 ;; Line containing content looks like:
 "            <span class=\"css-truncate css-truncate-target\"><a href=\"/yogthos/clojure-error-message-catalog/blob/master/clj/let-requires-even-number-forms.md\" class=\"js-navigation-open\" id=\"08e44a41930978c7addbd6e6153dffd7-bf4613ba2a9ed6522a518ab7d45baa4ac6d0a309\" title=\"let-requires-even-number-forms.md\">let-requires-even-number-forms.md</a></span>"
-
-(defn- only-md-lines [coll]
-  (filter #(s/includes? % ".md") coll))
 
 (defn- only-body [coll]
   (->> coll
@@ -20,9 +23,7 @@
   "Retrieve the list of errors from one of the subdirs of the github repo, as a list
   of maps, each of which contains the :href and :title keys."
   [dirname]
-  (->> (str
-        "https://github.com/yogthos/clojure-error-message-catalog/tree/master/"
-        dirname)
+  (->> (str base "/tree/master/" dirname)
        slurp
        h/parse
        h/as-hickory
@@ -33,8 +34,9 @@
          (hs/attr :title)))
        (map :attrs)))
 
+;; For now, just links direct to github -- later will slurp from github & embed
 (defn node-to-link [node]
-  [:a {:href (:href node)} (:title node)])
+  [:a {:href (str site (:href node))} (:title node)])
 
 (defn list-of-node-links [nodes]
   [:ul
